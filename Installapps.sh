b1c71b9ef46acb59960beb683579472c91cc3d8d
Matt Taylor <matthew.taylor@vanquisbank.co.uk>
2018-12-18 13:45:31 +0000

    lol

--- Installapps.sh
+++ Installapps.sh
@@ -30,5 +30,5 @@ helm init
 sudo apt-get install zsh
 sudo apt-get install git-core
 sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sudo chsh -s `which zsh`
+chsh -s `which zsh`
 sudo shutdown -r 0
